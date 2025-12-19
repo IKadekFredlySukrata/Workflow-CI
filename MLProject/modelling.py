a717e2634810222d7389818fc4488ab3d7283bc1
IKadekFredlySukrata <fredlysukrata@gmail.com>
2025-12-20 02:05:18 +0800

    Comment out mlflow experiment setup
    Commented out the mlflow experiment setting for testing.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -13,7 +13,7 @@ y = df["Class"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-mlflow.set_experiment("Wine_Classification_CI")
+#mlflow.set_experiment("Wine_Classification_CI")
 
 # FIXED: No fixed run_name
 with mlflow.start_run():
