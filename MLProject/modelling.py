04a86cd0a707c93ebcb9d05d2b4fe2741020e5ee
IKadekFredlySukrata <fredlysukrata@gmail.com>
2025-12-20 01:57:14 +0800

    Simplify MLflow run name in modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -24,7 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 mlflow.set_experiment("Wine_Classification_Experiment")
 
 # Start run
-with mlflow.start_run(run_name="RandomForest_Basic"):
+with mlflow.start_run():
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
 
