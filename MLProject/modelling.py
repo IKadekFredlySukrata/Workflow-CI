801d3518cc13e40928b6ea02a234b7e5ac1a2202
IKadekFredlySukrata <fredlysukrata@gmail.com>
2025-12-20 02:18:31 +0800

    Clean up mlflow experiment setup in modelling.py
    Removed commented-out experiment setting and fixed run name issue.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -13,9 +13,6 @@ y = df["Class"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-#mlflow.set_experiment("Wine_Classification_CI")
-
-# FIXED: No fixed run_name
 with mlflow.start_run():
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
